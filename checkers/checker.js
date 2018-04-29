68d65d5fe29e35ac2076c79ddfd2dfcabd93e79c
Uri Kalish <uri.kalish@hpe.com>
2018-04-29 14:25:36 +0300

    wip

--- checkers/checker.js
+++ checkers/checker.js
@@ -1,16 +1,20 @@
 'use strict';
+const dataProvider = require('../data/data-provider');
 const defectLargeAttachments = require('./defect-large-attachments');
 const defectManyComments = require('./defect-many-comments');
 const defectManyOwners = require('./defect-many-owners');
 const defectStuckPhase = require('./defect-stuck-phase');
 const defectUnusualOwner = require('./defect-unusual-owner');
 
-function check(defects) {
-	defectStuckPhase.check(defects);
-	defectUnusualOwner.check(defects);
-	defectManyOwners.check(defects);
-	defectManyComments.check(defects);
-	defectLargeAttachments.check(defects);
+function check() {
+	dataProvider.getDefects(100).then(
+	(defects) => {
+		defectStuckPhase.check(defects);
+		defectUnusualOwner.check(defects);
+		defectManyOwners.check(defects);
+		defectManyComments.check(defects);
+		defectLargeAttachments.check(defects);
+	});
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict';
 const authenticator = require('./data/authenticator');
-const dataProvider = require('./data/data-provider');
 const checker = require('./checkers/checker');
 
 function run() {
 	authenticator.authenticate().then(
 		(/*result*/) => {
 			console.log('Authenticated - OK');
-			dataProvider.getDefects(100).then(
-				(defects) => {
-					checker.check(defects);
-				});
+			checker.check();
 		},
 		(reason) => {
 			console.log('Authenticated Error - ' + reason);
