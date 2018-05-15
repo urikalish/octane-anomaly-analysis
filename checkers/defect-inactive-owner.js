a152cc9767084cf44193bcf9a047b6578031e586
Hanan Malka <hanan-israel.malka@hpe.com>
2018-05-15 17:17:37 +0300

    Adding anomalies

---
+++ checkers/defect-inactive-owner.js
@@ -0,0 +1,18 @@
+'use strict';
+const helper = require('../helper/helper');
+
+function check(defects, options) {
+    let defectsWithInactiveOwner = [];
+    defects.forEach(d => {
+        if (d.owner && d.owner.activity_level === 1 && options.phasesToIgnore.indexOf(d.phase.logical_name) === -1) {
+            defectsWithInactiveOwner.push(d);
+        }
+    });
+    defectsWithInactiveOwner.forEach(d => {
+        helper.logAnomaly(`Defect with inactive owner (${d.owner.full_name}) | ${helper.getDefectDetailsStr(d)}`);
+    });
+}
+
+module.exports = {
+    check: check
+};
