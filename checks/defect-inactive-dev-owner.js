a79217aba194dc45b716d2b942df1da1b46e82ea
Uri Kalish <uri.kalish@hpe.com>
2018-08-20 10:09:50 +0300

    wip

--- checks/defect-inactive-dev-owner.js
+++ checks/defect-inactive-dev-owner.js
@@ -11,6 +11,7 @@ function check(defects, options) {
 		defects.forEach(d => {
 			if (d.owner && d.owner.activity_level === 1 && options.phasesToIgnore.indexOf(d.phase.logical_name) === -1) {
 				result.anomalies[d.id] = {
+					checkerName: checkerName,
 					d: d,
 					text: `Defect with an inactive DEV owner (${d.owner.full_name}) | ${helper.getDefectDetailsStr(d)}`
                 };
