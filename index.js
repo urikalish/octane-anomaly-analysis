8b857de698058c3b9e25d8938af973a2e69c1fe8
Uri Kalish <uri.kalish@microfocus.com>
2019-04-15 10:03:59 +0300

    multiple tries

--- index.js
+++ index.js
@@ -3,19 +3,33 @@ const logger = require('./logger/logger');
 const octaneAuthenticator = require('./octane/octane-authenticator');
 const tagsManager = require('./tags/tags-manager');
 const defectsManager = require('./defects/defects-manager');
+const MAX_TRY_COUNT = 3;
+const WAIT_SECS_BETWEEN_TRIES = 5;
+let tryCount;
 
 let run = async () => {
-	let startTime = new Date();
+	tryCount++;
+	logger.logMessage(`Try number ${tryCount}`);
 	try {
-		logger.clearLog();
 		await octaneAuthenticator.authenticate();
 		logger.logSuccess('Authenticated - OK');
 		await tagsManager.loadUserTags();
+		let startTime = new Date();
 		await defectsManager.handleDefects();
 		logger.logSuccess(`Done - ${Math.round(((new Date()).getTime() - startTime.getTime()) / 1000)} seconds`);
 	} catch (err) {
 		logger.logError('Error');
+		if (tryCount <= MAX_TRY_COUNT) {
+			logger.logMessage(`Waiting for ${WAIT_SECS_BETWEEN_TRIES} seconds before trying again...`);
+			setTimeout(() => {
+				run();
+			}, WAIT_SECS_BETWEEN_TRIES*1000);
+		} else {
+			logger.logError(`Quit`);
+		}
 	}
 };
 
+logger.clearLog();
+tryCount = 0;
 run();
