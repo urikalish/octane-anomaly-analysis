bc9afb8597e903fa7d503971d57a0fee9a1814ee
Uri Kalish <uri.kalish@microfocus.com>
2019-12-24 19:43:55 +0200

    anomaly checkers for stories

---
+++ checks/story-stuck-dev-phase.js
@@ -0,0 +1,23 @@
+'use strict';
+const checkerName = require('path').basename(__filename).substring(0, require('path').basename(__filename).length - 3);
+const helper = require('../stories/stories-helper');
+
+const check = async (stories, options) => {
+	const result = helper.initCheckerResult(checkerName);
+	stories.forEach(s => {
+		if (s.phase
+		&& !options.phasesToIgnore.includes(s.phase.logical_name)
+		&& s['time_in_current_phase']) {
+			const maxDays = options.phasesMaxDays[s.phase.logical_name.toLowerCase()] || options.phasesMaxDays['_DEFAULT'];
+		 	const daysInCurrentPhase = Math.floor(s['time_in_current_phase'] / 1000 / 60 / 60 / 24);
+		 	if (daysInCurrentPhase > maxDays) {
+		 		helper.addStoryAnomaly(result, s, `Story stuck in DEV phase (${daysInCurrentPhase} days)`);
+		 	}
+		}
+	});
+	return result;
+};
+
+module.exports = {
+	check,
+};
